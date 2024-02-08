7a908a456a83b7b27979963ba9e0e62c6f8970f6
Niklas Röttger <niklas.roettger@outlook.de>
2024-02-08 09:57:29 +0100

    add fetch

--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,15 @@
-export default function List({ activities, isGoodWeather }) {
+export default function List({ activities, weather }) {
+  const { isGoodWeather, temperature, condition } = weather;
   return (
     <>
       <h2>
+        {condition} {temperature} °C
+      </h2>
+      <p>
         {isGoodWeather
           ? 'The weather is awesome! Go outside and:'
           : "Bad weather outside! Here's what you can do now:"}
-      </h2>
+      </p>
       <ul>
         {activities.map((activity) => {
           return <li key={activity.id}>{activity.name}</li>;
