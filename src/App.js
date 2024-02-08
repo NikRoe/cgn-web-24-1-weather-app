76f86dc778b4d9df99a50f7854066a821be91920
Niklas Röttger <niklas.roettger@outlook.de>
2024-02-08 09:32:53 +0100

    add activity list and local storage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
 import { uid } from 'uid';
+import List from './components/List';
+
+import useLocalStorageState from 'use-local-storage-state';
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useLocalStorageState('activites', {
+    defaultValue: [],
+  });
 
   function handleAddActivity(newActivity) {
     setActivities([...activities, { id: uid(), ...newActivity }]);
@@ -13,6 +17,7 @@ function App() {
   return (
     <>
       <h1>Recap Project 4</h1>
+      <List activities={activities} />
       <Form onAddActivity={handleAddActivity} />
     </>
   );
